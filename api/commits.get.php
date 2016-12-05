5fce2e146e7b4ae513aa20f0b9c8f8c51e8f98b7
plegall <plg@piwigo.org>
2016-12-05 11:22:26 +0100

    fixes #1, API commits.get.php
    With filters:
    * max_age (in seconds)
    * author
    * project
    * limit (max number of results)
    
    Commits are orders by date, from new to old.

---
+++ api/commits.get.php
@@ -0,0 +1,77 @@
+<?php
+include('../include/init.inc.php');
+
+$filter = array(
+  'max_age' => 30*24*60*60, // 30 days, in seconds
+  'author' => null,
+  'project' => null,
+  'limit' => null,
+  );
+
+if (isset($_GET['max_age']))
+{
+  $filter['max_age'] = (int)$_GET['max_age'];
+}
+
+if (isset($_GET['author']))
+{
+  $filter['author'] = $_GET['author'];
+}
+
+if (isset($_GET['project']))
+{
+  $filter['project'] = $_GET['project'];
+}
+
+if (isset($_GET['limit']))
+{
+  $filter['limit'] = (int)$_GET['limit'];
+}
+
+$query = '
+SELECT
+    project_id,
+    name,
+    url,
+    local_id,
+    author,
+    occured_on,
+    message
+  FROM commit
+    JOIN project ON project_idx = project_id
+  WHERE 1=1';
+
+if (isset($filter['max_age']))
+{
+  $query.= '
+    AND occured_on > SUBDATE(NOW(), INTERVAL '.$filter['max_age'].' SECOND)';
+}
+
+if (isset($filter['author']))
+{
+  $query.= '
+    AND author = "'.pwg_db_real_escape_string($filter['author']).'"';
+}
+
+if (isset($filter['project']))
+{
+  $query.= '
+    AND project = "'.pwg_db_real_escape_string($filter['project']).'"';
+}
+
+
+$query.= '
+  ORDER BY occured_on DESC';
+
+if (isset($filter['limit']))
+{
+  $query.= '
+  LIMIT '.$filter['limit'];
+}
+
+$query.= '
+;';
+$commits = query2array($query);
+
+echo json_encode($commits);
+exit();
